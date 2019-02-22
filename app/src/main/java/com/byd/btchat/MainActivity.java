7a174c7f33ee93a1f964f911e44a04fbe2378c67
jiang.xin3 <515519290@qq.com>
2019-02-22 17:17:31 +0800

    2019年2月22日

--- app/src/main/java/com/byd/btchat/MainActivity.java
+++ app/src/main/java/com/byd/btchat/MainActivity.java
@@ -1,13 +1,15 @@
 package com.byd.btchat;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+//https://blog.csdn.net/anddlecn/article/details/51880117
 public class MainActivity extends AppCompatActivity {
     private MenuItem mConnectionMenuItem;
+    private final int RESULT_CODE_BTDEVICE = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +30,9 @@ public class MainActivity extends AppCompatActivity {
             case R.id.connect_menu: {
                 //根据当前连接到状态，判断对应的响应方式，
                 //目前，我们就暂时将它设计成满足条件1的状况，
-                //启动DeviceListActivity获取可以连接到设备名称
+//                //启动DeviceListActivity获取可以连接到设备名称
+//                Intent i = new Intent(this, DeviceListActivity.class);
+//                startActivityForResult(i, RESULT_CODE_BTDEVICE);
 
             }
             return true;
